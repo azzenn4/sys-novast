4e49164c176cb37a20e41abb82951a6ba3893013
azzenn4 <69201270+azzenn4@users.noreply.github.com>
2025-01-21 14:45:44 +0700

    update filepath

--- metrics/_blackstar1_metrics.py
+++ metrics/_blackstar1_metrics.py
@@ -8,9 +8,22 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.ticker as ticker
 import numpy as np
 
-# Define the path to the trainer_state.json file
-checkpoint = 'checkpoint-3000'
-trainer_state_path = os.path.join(f'final_results_bs1', checkpoint, 'trainer_state.json')
+
+# Define the folder and checkpoint state
+folder = 'path/to/saved_checkpoint'  # Replace with the actual path
+state = 1000  # Replace with the correct checkpoint number
+checkpoint = f'checkpoint-{state}'
+
+# Construct the full path to trainer_state.json
+trainer_state_path = os.path.join(folder, checkpoint, 'trainer_state.json')
+
+# Check if the path exists
+if os.path.exists(trainer_state_path):
+    print("Checkpoint Found. Continue")
+else:
+    print("Path does not exist. Check the folder, state, or file.")
+
+
 themecolor = '#dae4f5'
 gridcolor = 'whitegrid'
 
