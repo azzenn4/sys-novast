f1fa886be4b06ec94eec7fd3caece259ef3648c2
azzenn4 <69201270+azzenn4@users.noreply.github.com>
2025-02-03 13:57:51 +0700

    Update CompositeDictionary.py

--- CompositeDictionary.py
+++ CompositeDictionary.py
@@ -1,1 +1,46 @@
 
+
+
+composite_emotions = {
+    "wistfulness which is bittersweet": {0, 1, 2},  # sadness + joy + love
+    "heartbreak_resentment which is anguished": {0, 2, 3},  # sadness + love + anger
+    "desperate_dread which is frantic": {0, 3, 4},  # sadness + anger + fear
+    "letdown_surprise which is disillusioned": {0, 1, 5},  # sadness + joy + surprise
+    "romantic_excitement which is passionate": {1, 2, 4},  # joy + love + fear
+    "victory_with_doubt which is conflicted": {1, 3, 4},  # joy + anger + fear
+    "delighted_awe which is wonder-filled": {1, 2, 5},  # joy + love + surprise
+    "possessive_jealousy which is intense": {2, 3, 4},  # love + anger + fear
+    "infatuated_surprise which is overwhelmed": {2, 5, 1},  # love + surprise + joy
+    "guarded_affection which is cautious": {2, 4, 5},  # love + fear + surprise
+    "resentful_fear which is defensive": {3, 4, 2},  # anger + fear + love
+    "shocked_betrayal which is traumatic": {3, 5, 0},  # anger + surprise + sadness
+    "fiery_hope which is optimistic": {3, 1, 2},  # anger + joy + love
+    "existential_tension which is crisis-driven": {4, 0, 3},  # fear + sadness + anger
+    "anxious_longing which is uncertain": {4, 2, 0},  # fear + love + sadness
+    "panic_joy which is jittery": {4, 1, 5},  # fear + joy + surprise
+    "startled_grief which is overwhelming": {5, 0, 4},  # surprise + sadness + fear
+    "elated_suspense which is thrilling": {5, 1, 4},  # surprise + joy + fear
+    "romantic_shock which is intense": {5, 2, 3},  # surprise + love + anger
+    "overwhelmed_loneliness which is isolating": {0, 2, 4},  # sadness + love + fear
+    "hopeful_dread which is uncertain": {4, 1, 0},  # fear + joy + sadness
+    "conflicted_satisfaction which is mixed": {1, 3, 2},  # joy + anger + love
+    "bitter_joy which is conflicted": {0, 1, 3},  # sadness + joy + anger
+    "angsty_love which is troubled": {0, 2, 4},  # sadness + love + fear
+    "elusive_happiness which is fleeting": {0, 1, 4},  # sadness + joy + fear
+    "frenzied_confusion which is chaotic": {3, 4, 5},  # anger + fear + surprise
+    "subdued_hope which is restrained": {0, 4, 2},  # sadness + fear + love
+    "jovial_melancholy which is bittersweet": {1, 0, 2},  # joy + sadness + love
+    "reluctant_affection which is hesitant": {2, 3, 5},  # love + anger + surprise
+    "anxiety_with_wonder which is anxious": {4, 1, 5},  # fear + joy + surprise
+    "intense_sorrow which is heavy": {0, 3, 4},  # sadness + anger + fear
+    "turbulent_joy which is conflicted": {1, 3, 4},  # joy + anger + fear
+    "wistful_euphoria which is bittersweet": {0, 1, 2},  # sadness + joy + love
+    "unsettled_exhilaration which is excited": {1, 4, 5},  # joy + fear + surprise
+    "overjoyed_confusion which is surprised": {1, 5, 3},  # joy + surprise + anger
+    "grieving_suspense which is sorrowful": {0, 4, 5},  # sadness + fear + surprise
+    "guilt_rage which is conflicted": {3, 0, 2},  # anger + sadness + love
+    "nervous_affection which is anxious": {4, 2, 1},  # fear + love + joy
+    "fascinated_dread which is tense": {4, 5, 3},  # fear + surprise + anger
+    "unsure_wonder which is confused": {0, 5, 4},  # sadness + surprise + fear
+    "peaceful_regret which is calm": {0, 2, 1},  # sadness + love + joy
+}
