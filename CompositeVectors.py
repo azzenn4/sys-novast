3651e97a0c5e453e0845328fe23cff2033cd8aee
azzenn4 <69201270+azzenn4@users.noreply.github.com>
2025-02-03 20:41:18 +0700

    Add files via upload

---
+++ CompositeVectors.py
@@ -0,0 +1,66 @@
+CompositeDictionary = {
+    "bittersweet": {
+        'emotions': [0, 1, 2],  # sadness + joy + love
+        'weights': [0.615, 0.615, 0.525]  # moderate amplification
+    },
+    "psychological anguish": {
+        'emotions': [0, 2, 3],  # sadness + love + anger
+        'weights': [0.785, 0.615, 0.35]  # moderate amplification
+    },
+    "frantic fear response": {
+        'emotions': [0, 3, 4],  # sadness + anger + fear
+        'weights': [0.525, 0.70, 0.525]  # moderate amplification
+    },
+    "cognitive disillusionment": {
+        'emotions': [0, 1, 5],  # sadness + joy + surprise
+        'weights': [0.70, 0.44, 0.615]  # moderate amplification
+    },
+    "euphoric passion": {
+        'emotions': [1, 2, 4],  # joy + love + fear
+        'weights': [0.70, 0.70, 0.35]  # moderate amplification
+    },
+    "ambivalent joy": {
+        'emotions': [1, 3, 4],  # joy + anger + fear
+        'weights': [0.785, 0.525, 0.4375]  # moderate amplification
+    },
+    "conflicted happiness": {
+        'emotions': [0, 1, 3],  # sadness + joy + anger
+        'weights': [0.525, 0.7875, 0.4375]  # moderate amplification
+    },
+    "bittersweet contentment": {
+        'emotions': [1, 0, 2],  # joy + sadness + love
+        'weights': [0.70, 0.70, 0.35]  # moderate amplification
+    },
+    "state of nervous tension": {
+        'emotions': [1, 4, 5],  # joy + fear + surprise
+        'weights': [0.4375, 0.70, 0.525]  # moderate amplification
+    },
+    "nostalgic affection": {
+        'emotions': [0, 1, 2],  # sadness + joy + love
+        'weights': [0.525, 0.525, 0.70]  # moderate amplification
+    },
+    "wounded indignation": {
+        'emotions': [0, 3, 4],  # sadness + anger + fear
+        'weights': [0.785, 0.615, 0.35]  # moderate amplification
+    },
+    "aesthetic amazement": {
+        'emotions': [1, 2, 5],  # joy + love + surprise
+        'weights': [0.70, 0.4375, 0.615]  # moderate amplification
+    },
+    "intense romantic passion": {
+        'emotions': [2, 3, 4],  # love + anger + fear
+        'weights': [0.525, 0.875, 0.35]  # moderate amplification
+    },
+    "tentative hope": {
+        'emotions': [1, 4, 5],  # joy + fear + surprise
+        'weights': [0.6125, 0.70, 0.4375]  # moderate amplification
+    },
+    "careful compassion": {
+        'emotions': [1, 2, 4],  # joy + love + fear
+        'weights': [0.70, 0.6125, 0.4375]  # moderate amplification
+    },
+    "shock-induced devastation": {
+        'emotions': [0, 4, 5],  # sadness + fear + surprise
+        'weights': [0.875, 0.4375, 0.4375]  # moderate amplification
+    }
+}
